5cfd5ab9f7ab55254b40304ed02a1fa7ec5cc7ab
sclin87 <mikelolct.cs07@nctu.edu.tw>
2022-05-13 20:14:26 +0800

    Test Simple Discounts redlight

--- test.py
+++ test.py
@@ -15,3 +15,15 @@ def testBasics():
     assert 8 == customer.checkout()
     customer.buyList([1, 1, 1])
     assert 8 * 3 == customer.checkout()
+
+
+def testSimpleDiscounts():
+    customer = Customer()
+    customer.buyList([0, 1])
+    assert 8 * 2 * 0.95 == customer.checkout()
+    customer.buyList([0, 2, 4])
+    assert 8 * 3 * 0.9 == customer.checkout()
+    customer.buyList([0, 1, 2, 4])
+    assert 8 * 4 * 0.8 == customer.checkout()
+    customer.buyList([0, 1, 2, 3, 4])
+    assert 8 * 5 * 0.75 == customer.checkout()
